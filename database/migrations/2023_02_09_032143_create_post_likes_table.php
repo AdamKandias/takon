6ff7380ef6ebf848a580780f85388a146c4a489b
AdamKandias <adamkandias31@gmail.com>
2023-02-09 10:31:37 +0700

    create post_likes table and post_reports table

---
+++ database/migrations/2023_02_09_032143_create_post_likes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_likes', function (Blueprint $table) {
+            $table->foreignUuid("post_id");
+            $table->foreign("post_id")->references("id")->on("posts");
+            $table->foreignId("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->primary(['post_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post_likes', function (Blueprint $table) {
+            $table->dropForeign(['post_id']);
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('post_likes');
+    }
+};
---
+++ database/migrations/2023_02_09_032205_create_post_reports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_reports', function (Blueprint $table) {
+            $table->foreignUuid("post_id");
+            $table->foreign("post_id")->references("id")->on("posts");
+            $table->foreignId("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->primary(['post_id', 'user_id']);
+            $table->string("description", 100)->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post_reports', function (Blueprint $table) {
+            $table->dropForeign(['post_id']);
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('post_reports');
+    }
+};
