6019a31b676b4ce56552c015f1cfc6a003e8b376
AdamKandias <adamkandias31@gmail.com>
2023-02-09 09:43:33 +0700

    make follows migration

---
+++ database/migrations/2023_02_09_022735_create_follows_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->foreignId("following_user_id");
+            $table->foreign("following_user_id")->references("id")->on("users");
+            $table->foreignId("followed_user_id");
+            $table->foreign("followed_user_id")->references("id")->on("users");
+            $table->primary(['following_user_id', 'followed_user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('follows', function (Blueprint $table) {
+            $table->dropForeign(['following_user_id']);
+            $table->dropForeign(['followed_user_id']);
+        });
+        Schema::dropIfExists('follows');
+    }
+};
