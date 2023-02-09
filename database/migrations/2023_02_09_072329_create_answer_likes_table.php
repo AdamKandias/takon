77b73881a4b22e7fa80f4d8e443636f278b23245
AdamKandias <adamkandias31@gmail.com>
2023-02-09 14:29:27 +0700

    create answer_likes migration and create answer_reports migration

---
+++ database/migrations/2023_02_09_072329_create_answer_likes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer_likes', function (Blueprint $table) {
+            $table->foreignUuid("answer_id");
+            $table->foreign("answer_id")->references("id")->on("answers");
+            $table->foreignId("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->primary(['answer_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answer_likes', function (Blueprint $table) {
+            $table->dropForeign(['answer_id']);
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('answer_likes');
+    }
+};
---
+++ database/migrations/2023_02_09_072359_create_answer_reports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer_reports', function (Blueprint $table) {
+            $table->foreignUuid("answer_id");
+            $table->foreign("answer_id")->references("id")->on("answers");
+            $table->foreignId("user_id");
+            $table->foreign("user_id")->references("id")->on("users");
+            $table->primary(['answer_id', 'user_id']);
+            $table->string("description", 100)->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answer_reports', function (Blueprint $table) {
+            $table->dropForeign(['answer_id']);
+            $table->dropForeign(['user_id']);
+        });
+        Schema::dropIfExists('answer_reports');
+    }
+};
